2f4e7ba1c9c289aef1989af38e34732a35509f6d
elielvipata <vkilembo@gmail.com>
2024-12-13 23:51:29 -0500

    base server, receives request and sends response. next parsing the request from the browser and having different endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7778").unwrap();
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
 }
+
+fn handle_connection(mut stream: TcpStream){
+    let buf_reader = BufReader::new(&stream);
+    let http_request:Vec<_> = buf_reader
+       .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+
+    handle_okresponse(stream);
+
+    println!("Response sent!");
+
+}
+
+
+fn handle_okresponse(mut stream: TcpStream){
+    let ok_response = "HTTP/1.1 200 OK\r\n";
+    let contents = fs::read_to_string("index.html").unwrap();
+    let length = contents.len();
+
+    let entire_body =format!("{ok_response}Content-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(entire_body.as_bytes()).unwrap();
+}
+
